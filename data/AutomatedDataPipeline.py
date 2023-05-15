70f9c6784bee5bdf374c3ca1321445a74315d5c8
Carsten Schmotz <carsten.schmotz@fau.de>
2023-05-15 13:33:27 +0200

    automated car registration

--- data/AutomatedDataPipeline.py
+++ data/AutomatedDataPipeline.py
@@ -5,31 +5,33 @@ downloadFiles = True
 storage_options = {'User-Agent': 'Mozilla/5.0'}
 
 #latitude,longitude,speed,shock_duration,shock_x_axis,x_axis,shock_y_axis,y_axis,shock_z_axis,z_axis,wagonID,delta_timestamp
-#if(downloadFiles):
+if(downloadFiles):
     #df = pd.read_csv('https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ28/fz28_2022_09.xlsx?__blob=publicationFile&v=4', sep=',', storage_options=storage_options, nrows=15, usecols=["latitude", "longitude", "speed"])
- #   df = pd.read_excel('https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ28/fz28_2022_09.xlsx?__blob=publicationFile&v=4', index_col=None, header=None) 
-#else:
- #   df = pd.read_csv('C:/Users/nicol/Downloads/ShockData.csv', sep=',', nrows=15, usecols=["latitude", "longitude", "speed"])
+    df = pd.read_excel('https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ28/fz28_2022_09.xlsx?__blob=publicationFile&v=4', sheet_name=4,header=[7,8,9,10,11]) #skiprows=[0,1,2,3,4,5,6,101,102]) 
+# >>> pd.read_excel('tmp.xlsx', index_col=0, comment='#')  #try this
+ #use col machen und header entfernen
+else:
+   df = pd.read_csv('C:/Users/nicol/Downloads/ShockData.csv', sep=',', nrows=15, usecols=["latitude", "longitude", "speed"])
 
 #df.columns.values[0] = 'lat'
 #df.columns.values[1] = 'lon'
 #df.columns.values[2] = 'speed2'
 
-#df.to_sql('carregistration', 'sqlite:///CarRegistration.sqlite', if_exists='replace', index=False)
+df.to_sql('carregistration', 'sqlite:///CarRegistration.sqlite', if_exists='replace', index=False)
 
-#print("First DONE ")
+print("First DONE ")
 
-if(downloadFiles):
-    df = pd.read_csv('https://www-genesis.destatis.de/genesis/downloads/00/tables/61243-0002_00.csv', sep=';', engine='python')#skiprows =[0,1,2,3,4,5])
+#if(downloadFiles):
+ #   df = pd.read_csv('https://www-genesis.destatis.de/genesis/downloads/00/tables/61243-0002_00.csv', sep=';', engine='python')#skiprows =[0,1,2,3,4,5])
 #usecols=['Strompreise für Haushalte: Deutschland', 'Jahre',
 #'Jahresverbrauchsklassen', 'Preisbestandteile',
 #'Durchschnittspreise für Strom und Gas'
 #,'Deutschland'])
-else:
-    df = pd.read_csv('/Users/carstenschmotz/Downloads/61243-0002_00.csv', sep=';', skiprows=[0,1,2,3,4,5])
+#else:
+ #   df = pd.read_csv('/Users/carstenschmotz/Downloads/61243-0002_00.csv', sep=';', skiprows=[0,1,2,3,4,5])
 #/Users/carstenschmotz/Downloads/61243-0002_00.csv
 #/Users/carstenschmotz/Documents/GitHub/2023-AMSE-cs/data
-df.to_sql('prize', 'sqlite:///Energyprize.sqlite', if_exists='replace', index=False)
+#df.to_sql('prize', 'sqlite:///Energyprize.sqlite', if_exists='replace', index=False)
 #df.to_sql('prize', 'sqlite:///Users/carstenschmotz/Documents/GitHub/2023-AMSE-cs/data/prize.sqlite', if_exists='replace', index=False)
 
 print("Second DONE")
