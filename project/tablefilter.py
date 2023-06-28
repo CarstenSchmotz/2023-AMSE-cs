90dd89678a88e1aa85fd78a23081574f99451518
Carsten Schmotz <carsten.schmotz@fau.de>
2023-06-28 16:50:50 +0200

    change back

---
+++ project/tablefilter.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import sqlite3
+from sqlalchemy.types import Integer, FLOAT,String
+
+carreg_table = "./CarRegistration.sqlite"
+energy_table = "./Energyprize.sqlite"
+
+
+
+
+
+
+#Car registrations
+conn_reverse = sqlite3.connect(carreg_table)
+cursor = conn_reverse.cursor()
+
+#Reverse data to match
+sql_query_reverse = '''
+SELECT * FROM carregistration
+ORDER BY rowid DESC
+'''
+df = pd.read_sql_query(sql_query_reverse, conn_reverse)
+df.to_sql('carregistration',conn_reverse, if_exists= 'replace', index= False)
+conn_reverse.close
+
+
+
+
+
+conn = sqlite3.connect(carreg_table)
+cursor = conn.cursor()
+
+#Filter the sums of the years
+teilstring = 'Jahr'
+sql_query = '''
+SELECT * FROM Cars
+where Monat LIKE '%{}%' 
+'''.format(teilstring)
+df = pd.read_sql_query(sql_query, conn)
+conn.close
+
+Result = './data.sqlite'
+conn_neu = sqlite3.connect(Result)
+df.to_sql('Cars',conn_neu, if_exists= 'replace', index= False)
+conn_neu.close
+
+print('Carfilter done')   
+    
+    
+    
+
+
+
+
+
+
+#Energy prizes
+conn_prize = sqlite3.connect(energy_table)
+cursor = conn_prize.cursor()
+
+#Reverse data to match
+sql_query_reverse = '''
+SELECT * FROM prize
+ORDER BY rowid DESC
+'''
+df = pd.read_sql_query(sql_query_reverse, conn_prize)
+df.to_sql('prize',conn_prize, if_exists= 'replace', index= False)
+conn_prize.close
+
+
+
+conn = sqlite3.connect(energy_table)
+cursor = conn.cursor()
+
+#Filter the sums of the years
+sql_query = '''
+SELECT * FROM Prize
+where Haushalte = 'Insgesamt' '''
+
+df = pd.read_sql_query(sql_query, conn)
+conn.close
+
+conn_neu = sqlite3.connect(Result)
+df.to_sql('Prize',conn_neu, if_exists= 'replace', index= False)
+conn_neu.close
+
+print('Energyfilter done')
+
+
+
+
+
