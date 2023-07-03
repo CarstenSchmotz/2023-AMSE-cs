4c6a6b7fda3eb54b34d50db44e589cedaee787db
Carsten Schmotz <carsten.schmotz@fau.de>
2023-07-03 15:08:31 +0200

    test

--- project/testing.py
+++ project/testing.py
@@ -1,17 +1,31 @@
 import pandas as pd
 import os
 
+
+
+
+# Aktuelles Arbeitsverzeichnis erhalten
+current_dir = os.getcwd()
+#print(current_dir)
+
+# Relativer Pfad zur AutomatedDataPipeline.py
+relative_path_pipeline = "../data/AutomatedDataPipeline.py"
+#pathtablefilter = "../data/tablefilter.py" 
+
+# Absoluter Pfad berechnen
+absolute_path = os.path.join(current_dir, relative_path_pipeline)
+#absolute_path_filter = os.path.join(current_dir, pathtablefilter)
 #Get the right paths
 pathcar = "./CarRegistration.sqlite"
 pathenergy = "./Energyprize.sqlite"
-pathAutomatedDatapipeline = "./main/AutomatedDataPipeline.py"
+pathAutomatedDatapipeline = "./data/AutomatedDataPipeline.py"#pfad checken
 print(os.path.abspath(pathAutomatedDatapipeline))
 print("Filepath found")
 
     
 #Run datapipeline
 def runAutomatedDatapPipeline():
-    command = "python " + os.path.abspath(pathAutomatedDatapipeline)
+    command = "python " + os.path.abspath(absolute_path)
     os.system(command)
     
 #Test path     
